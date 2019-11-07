ff000e39e1e7761922a1fb7c3d12f46b711fcb6f
AkiraK <hynicalsphere@gmail.com>
2019-11-07 21:43:29 +0900

    文言修正

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Portrait of Akira.K' });
+  res.render('index', { title: 'Portfolio of Akira.K' });
 });
 
 router.get('/about', function(req, res, next){
